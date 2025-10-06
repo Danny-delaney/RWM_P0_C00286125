f5735f34bdeb2db797f47091c8b7f7473d30456c
CD-L216-303\gameuser <c00286125@setu.ie>
2025-10-06 11:06:19 +0100

    step 1

--- Sprint 6/tests/checklist.spec.js
+++ Sprint 6/tests/checklist.spec.js
@@ -1,7 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-// This test navigates to /lab/checklist and asserts the title
-
 test('Checklist page displays correct title', async ({ page }) => {
   await page.goto('/lab/checklist');
   const title = await page.getByTestId('title');
